66581f174985372ee6b31d73cc93b1a0b170bc6b
bananacabana <nwhays27@gmail.com>
2020-02-03 13:03:10 -0600

    decided to use my big brain and style and framework myself. Got the data to log to the console. Next step is to display the data on html

--- script.js
+++ script.js
@@ -1,79 +1,15 @@
+var button = document.querySelector('.button')
+var inputValue = document.querySelector('.inputValue')
+var name = document.querySelector('.name')
+var description = document.querySelector('.description')
+var temp = document.querySelector('.temp')
 
-var tempMode = 1;
+button.addEventListener('click', function() {
 
+  fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=210c3c7bad5a6c9c82c3686871ddecf6')
+  .then(response => response.json())
+  .then(data => console.log(data))
 
+  .catch(err => alert("Invalid city name!"))
+})
 
-function getWeather(lat, lon) {
-  var apiURI = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=06170c100199dbae1e223cc3dfad960b";
-
-  $.ajax({
-    url: apiURI,
-    dataType: "json",
-    type: "GET",
-    async: "false",
-    success: function(resp) {
-
-      $("#tempMode").on("click", function() {
-        if (this.checked) {
-          $("#temp-text").html(cels.toFixed(1) + " C&deg");
-          console.log("checked");
-        } else
-          $("#temp-text").html(fahr.toFixed(0) + " F&deg");
-      });
-      console.log(apiURI);
-      console.log(resp.name);
-      if (resp.name) {
-        $("#city-text").html(resp.name + ", " + resp.sys.country);
-      }
-      if (resp.wind) {
-        var knots = resp.wind.speed * 1.9438445;
-        $("#wind-text").html(knots.toFixed(1) + " Knots");
-      }
-      if (resp.main.temp) {
-        var fahr = (resp.main.temp * 9 / 5) - 459.67;
-        var cels = (resp.main.temp - 273.15);
-        if (cels > 24){ 
-          $("#temp-text").css("color", "red");
-        } else if (cels < 18){
-          $("#temp-text").css("color", "blue");
-        }
-        $("#temp-text").html((tempMode === 1 ? fahr.toFixed(0) + " F&deg" : cels.toFixed(0) + " C&deg"));
-      }
-      if (resp.weather) {
-        var imgURL = "http://openweathermap.org/img/w/" + resp.weather[0].icon + ".png";
-        console.log(imgURL)
-        $("#weatherImg").attr("src", imgURL);
-        $("#weather-text").html(resp.weather[0].description);
-      }   
-    },
-    error: function(resp) {
-       alert("Error: " + e);
-       clearInterval(updateinter);
-    }
-  });
-}
-
-function getLocation() {
-  if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      getWeather(position.coords.latitude, position.coords.longitude);
-    })
-  } else {
-    alert("geolocation not available" + e);
-    clearInterval(updateinter);
-  }
-}
-var i = 0;
-var updateinter = setInterval(function(){
-  console.log("iteration# " + i++);
-  if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      getWeather(position.coords.latitude, position.coords.longitude);
-    })
-  } else {
-    alert("geolocation not available" + e);
-  }
-}, 300000);
-
-
-getLocation();
