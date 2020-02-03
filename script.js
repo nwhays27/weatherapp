c155f2e8774454a7e13794885b70a9a051753b54
bananacabana <nwhays27@gmail.com>
2020-02-03 17:11:38 -0600

    When city is searched it displays condition, temp, wind, humidity. Next is 5 day forecast.

--- script.js
+++ script.js
@@ -3,20 +3,29 @@ var inputValue = document.querySelector('.inputValue')
 var name = document.querySelector('.name')
 var description = document.querySelector('.description')
 var temp = document.querySelector('.temp')
+var wind = document.querySelector('.wind')
+var humidity = document.querySelector('.humidity')
 
 button.addEventListener('click', function() {
-
+  
   fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=210c3c7bad5a6c9c82c3686871ddecf6')
+
   .then(response => response.json())
   .then(data => {
     var nameValue = data['name'];
     var tempValue =data['main']['temp'];
     var descriptionValue = data['weather'][0]['description'];
+    var windValue = data['wind']['speed'];
+    var humidityValue = data['main']['humidity'];
 
+    
     name.innerHTML = nameValue;
-    temp.innerHTML = tempValue;
-    description.innerHTML = descriptionValue;
-
+    temp.innerHTML = "Temperature: " + (1.8*(tempValue-273)+32) + " F" ;
+    description.innerHTML ="Current Conditions: " +  descriptionValue;
+    wind.innerHTML = "Wind Speed: " + windValue + "mph";
+    humidity.innerHTML = "Humidity: " + humidityValue + "%";
+    
+    console.log(data)
   })
 
 
