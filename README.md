ec00f79cb8632b626d471b1fa61a412597812a61
bananacabana <nwhays27@gmail.com>
2020-02-05 10:36:18 -0600

    updated readme. debugged a problem retrieving the humidity and wind speed.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # weatherapp
-5 day weather forecast
+
+This application will return the current weather for whatever city you search.
+
+I'm using the Open Weather API for this application. Created variables to grab the data from the object and display them in html.
--- script.js
+++ script.js
@@ -6,11 +6,15 @@ var temp = document.querySelector('.temp')
 var wind = document.querySelector('.wind')
 var humidity = document.querySelector('.humidity')
 
-button.addEventListener('click', function() {
-  
+
+
+  button.addEventListener('click', function() {
+  //API Call. Find the value of what the user input into the search bar
   fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=210c3c7bad5a6c9c82c3686871ddecf6')
+  
 
   .then(response => response.json())
+  //Created variables to grab the data
   .then(data => {
     var nameValue = data['name'];
     var tempValue =data['main']['temp'];
@@ -18,17 +22,17 @@ button.addEventListener('click', function() {
     var windValue = data['wind']['speed'];
     var humidityValue = data['main']['humidity'];
 
+  //Converting the data to text/html
     
     name.innerHTML = nameValue;
     temp.innerHTML = "Temperature: " + (1.8*(tempValue-273)+32) + " F" ;
     description.innerHTML ="Current Conditions: " +  descriptionValue;
     wind.innerHTML = "Wind Speed: " + windValue + "mph";
     humidity.innerHTML = "Humidity: " + humidityValue + "%";
-    
+  
     console.log(data)
   })
 
 
   .catch(err => alert("Invalid city name!"))
-})
-
+});
